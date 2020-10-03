51c9a8d92411ec2fa96a724dd4f783a24fac4df6
yael <yaeliola@gmail.com>
2020-10-03 17:01:18 +0300

    getting styling settings

--- player.js
+++ player.js
@@ -1,4 +1,8 @@
+function getCSSContent(json){
+    $("<style type='text/css'>" + json.data.css + "</style>").appendTo("head");
+}
 $.getJSON("https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=?",function(json){
+    getCSSContent(json);    
     console.log(json);
     });
 
